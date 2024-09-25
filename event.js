43b585e658266c08fdc6ba61ad85108ba013365c
Kiran Kumar <kirankumarpilli82gmail.com>
2024-09-25 20:28:14 +0530

    hii

---
+++ event.js
@@ -0,0 +1,30 @@
+function fn1(){
+
+var h1=document.getElementsByClassName("kiran6")
+var h2=document.getElementsByClassName("kiran1")
+h1[0].setAttribute("src",h2[0].src)
+}
+function fn2(){
+
+    var h1=document.getElementsByClassName("kiran6")
+    var h2=document.getElementsByClassName("kiran2")
+    h1[0].setAttribute("src",h2[0].src)
+    }
+    function fn3(){
+
+        var h1=document.getElementsByClassName("kiran6")
+        var h2=document.getElementsByClassName("kiran3")
+        h1[0].setAttribute("src",h2[0].src)
+        }
+        function fn4(){
+
+            var h1=document.getElementsByClassName("kiran6")
+            var h2=document.getElementsByClassName("kiran4")
+            h1[0].setAttribute("src",h2[0].src)
+            }
+      function fn5(){
+        var kirann=document.getElementsByClassName("kiran6")
+        var kirann1=document.getElementsByClassName("kiran5")
+        kirann[0].setAttribute("src",kirann1[0].src)
+      }
+      
